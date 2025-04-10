8bdccf4587d5d7ff5fee95de658d72bd716c2175
Rishabh Singh <33364895+Rishabh1839@users.noreply.github.com>
2025-04-09 19:24:32 -0700

    initial commit

---
+++ Java_DSA/Main.java
@@ -0,0 +1,25 @@
+import java.util.Queue;
+
+import java.util.LinkedList;
+public static void main (String[] args){
+    // Create a queue of strings
+    Queue<String> queue = new LinkedList<String>();
+
+    // Add elements to the queue
+    queue.offer("Karen");
+    queue.offer("Chad");
+    queue.offer("Steve");
+    queue.offer("Harold");
+
+    //System.out.println(queue.isEmpty()); // Output: []
+    System.out.println(queue.size()); // Output: 4
+    System.out.println(queue.contains("Harold"));
+
+    //System.out.println(queue.peek());
+    //queue.poll(); // Remove the first element (Karen)
+    //queue.poll();
+    //queue.poll();
+    //queue.poll();
+
+    System.out.println(queue); // Output: [Karen, Chad, Steve, Harold]
+}
